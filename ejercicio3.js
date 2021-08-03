2c9280614ed5f05d12a04b3ab15e5c9e55f74fe2
danybarrera952 <danielbarrera952@hotmail.com>
2021-08-03 14:27:31 -0500

    tercer ejercicio

---
+++ ejercicio3.js
@@ -0,0 +1,29 @@
+function concidencias(frase, palabra) {
+  let convertTexttoLowerCase;
+  let convertpalabratolowerCase;
+  let arrayFrase = [];
+  let count = 0;
+  //expresion regular
+  convertTexttoLowerCase = frase.toLowerCase().replace(/[!,.-]/g, '');
+
+  convertpalabratolowerCase = palabra.toLowerCase();
+
+  if (!convertTexttoLowerCase.includes(convertpalabratolowerCase))
+    return `Cero concidencias para esa palabras`;
+
+  arrayFrase = convertTexttoLowerCase.split(' ');
+
+  for (let i = 0; i <= arrayFrase.length; i++) {
+    if (arrayFrase[i] === convertpalabratolowerCase) {
+      count = count + 1;
+    }
+  }
+  return `La palabra: ${convertpalabratolowerCase} se encontro ${count} en la frase: ${convertTexttoLowerCase}`;
+}
+
+console.log(
+  concidencias(
+    'Vamos rayados, vamos. RaYADOS vamos vamos abuela!!....',
+    'abuela'
+  )
+);
